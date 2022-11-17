c9c0211f9a62c1afd6ca8e3c041e9dbbd223c707
Anastasiya <test@mail.test>
2022-11-17 09:26:34 +0200

    task implementation

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -1,11 +1,16 @@
 package com.epam.rd.autotasks;
 
+import java.util.Arrays;
+
 class CycleSwap {
     static void cycleSwap(int[] array) {
-
+        cycleSwap(array, 1);
     }
 
-    static void cycleSwap(int[] array, int shift) {
-
+   static void cycleSwap(int[] array, int shift) {
+        int[] arr2 = Arrays.copyOf(array, array.length);
+        for (int i = 0; i < array.length; i++) {
+            array[i] = arr2[(array.length + i - shift) % array.length];
+        }
     }
 }
